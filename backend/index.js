2010e37a006415fba12db65f0bdb0e8f7dfedf6c
barsha5 <ummehbarsha@gmail.com>
2025-04-22 21:27:29 -0400

    Docker container built

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
-const app = require('./app');
+// const app = require('./app');
 
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
